9c62f3cb31b6fe44b844c3d77af75f89deb832a3
Premanandhan Narayanan <premanandhan@gmail.com>
2026-07-28 21:29:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 // Create Map
-
 const map = L.map('map').setView([13.0827, 80.2707], 10);
 
-// OpenStreetMap Tiles
-
+// OpenStreetMap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-
-    maxZoom:19,
-
-    attribution:'© OpenStreetMap'
-
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap'
 }).addTo(map);
+
+// Load JSON
+fetch('data/schools.json')
+    .then(response => response.json())
+    .then(schools => {
+
+        console.log(schools);
+
+    })
+    .catch(error => console.error(error));
